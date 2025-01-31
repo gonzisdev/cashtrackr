8de0bfb728bad6b757b409b4caee393ff76e8ce0
Gonzalo Vasco López <gvascolopez@gmail.com>
2025-01-31 10:07:02 +0100

    feat: implement budget creation in BudgetController with error handling and unit tests

--- backend/src/tests/unit/BudgetController.test.ts
+++ backend/src/tests/unit/BudgetController.test.ts
@@ -4,7 +4,8 @@ import { BudgetController } from '../../controllers/BudgetController'
 import Budget from '../../models/Budget'
 
 jest.mock('../../models/Budget', () => ({
-  findAll: jest.fn(() => budgets)
+  findAll: jest.fn(),
+  create: jest.fn()
 }))
 
 describe('BudgetController.getAll', () => {
@@ -74,5 +75,56 @@ describe('BudgetController.getAll', () => {
     await BudgetController.getAll(req, res)
     expect(res.statusCode).toBe(500)
     expect(res._getJSONData()).toEqual({ error: 'Hubo un error' })
+  })
+})
+
+describe('BudgetController.create', () => {
+  it('should create a new budget and respond with statusCode 201', async () => {
+    const mockBudget = {
+      save: jest.fn().mockResolvedValue(true)
+    }
+    ;(Budget.create as jest.Mock).mockResolvedValue(mockBudget)
+    const req = createRequest({
+      method: 'POST',
+      url: '/api/budgets',
+      user: { id: 1 },
+      body: {
+        name: 'Presupuesto prueba',
+        amount: 1000
+      }
+    })
+    const res = createResponse()
+    await BudgetController.create(req, res)
+
+    const data = res._getJSONData()
+    expect(res.statusCode).toBe(201)
+    expect(data).toBe('Presupuesto creado correctamente')
+    expect(mockBudget.save).toHaveBeenCalled()
+    expect(mockBudget.save).toHaveBeenCalledTimes(1)
+    expect(Budget.create).toHaveBeenCalledWith(req.body)
+  })
+
+  it('should handle budget creation error', async () => {
+    const mockBudget = {
+      save: jest.fn()
+    }
+    ;(Budget.create as jest.Mock).mockRejectedValue(new Error())
+    const req = createRequest({
+      method: 'POST',
+      url: '/api/budgets',
+      user: { id: 1 },
+      body: {
+        name: 'Presupuesto prueba',
+        amount: 1000
+      }
+    })
+    const res = createResponse()
+    await BudgetController.create(req, res)
+
+    const data = res._getJSONData()
+    expect(res.statusCode).toBe(500)
+    expect(data).toEqual({ error: 'Hubo un error' })
+    expect(mockBudget.save).not.toHaveBeenCalled()
+    expect(Budget.create).toHaveBeenCalledWith(req.body)
   })
 })
